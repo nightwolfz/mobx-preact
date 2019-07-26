6db09a44ed597057b093fd273d981fe2a54f0ff0
Anton Adamansky <adamansky@gmail.com>
2019-07-26 19:18:42 +0700

    * Types updated for inject, connect

--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,12 @@
-declare module 'mobx-preact' {
+declare module '@softmotions/mobx-preact' {
   import { ComponentConstructor } from 'preact';
-  type Component<P = any> = ComponentConstructor<P>;
+  type ComponentClass<P = any> = ComponentConstructor<P>;
 
-  export function observer<T extends Component>(target: T): T;
+  export function observer<T extends ComponentClass>(target: T): T;
+
+  export function inject(...stores: string[]): <T extends ComponentClass>(target: T) => T;
+
+  export function inject(sfn: Function): <T extends ComponentClass>(target: T) => T;
+
+  export function connect(stores: string[]): <T extends ComponentClass>(target: T) => T;
 }
